5e199a8ebcf2bb6fd1bb7ff2763424915a05bf59
Stephanie Shea <stephcshea@gmail.com>
2017-11-25 19:56:13 +0000

    Tidied cod, reinforced understanding

--- objects.js
+++ objects.js
@@ -1,18 +1,16 @@
-var hotel  = {
-name: "Biltmore",
-rooms: 500,
-roomsBooked: 150,
-roomService: true,
-pool: false,
-
-roomsAvail: function() {
-    return this.rooms - this.roomsBooked;
-}
+var hotel = {
+	name: "Biltmore",
+	rooms: 500,
+	roomsBooked: 150,
+	roomService: true,
+	pool: false,
+	roomsAvail: function() {
+		return this.rooms - this.roomsBooked;
+	}
 };
 
-function tah () {
-    document.getElementById('name').innerHTML = hotel.name;
-    document.getElementById('rooms').innerHTML = hotel.rooms;
-    document.getElementById('avail').innerHTML = hotel.roomsAvail();
+function tah() {
+	document.getElementById('name').innerHTML = hotel.name;
+	document.getElementById('rooms').innerHTML = hotel.rooms;
+	document.getElementById('avail').innerHTML = hotel.roomsAvail();
 };
-    
